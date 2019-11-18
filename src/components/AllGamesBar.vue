8913644b63135304ca1945e3d0452c2051c9edab
zhoricc3398 <16200126@ibsu.edu.ge>
2019-11-18 22:52:55 +0400

    first commit

---
+++ src/components/AllGamesBar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="all-games-bar">
+    <div class="all-games-bar__search-container">
+      <label>
+        <input type="text" placeholder="Search Events...." />
+      </label>
+      <i class="material-icons">search</i>
+    </div>
+    <div class="all-games-bar__top">
+      <h6 class="all-games-bar__title">All Games<span>2328</span></h6>
+      <div class="all-games-bar__list-wrap">
+        <p
+          class="all-games-bar__list-item"
+          v-for="(item, index) in sportService.EN.Sports"
+          v-bind:key="index"
+        >
+          {{ item.Name }}<span>{{ item.Regions }}</span>
+        </p>
+      </div>
+    </div>
+    <div class="all-games-bar__bottom"></div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  mounted() {
+    this.$store.dispatch("getSportService");
+  },
+  computed: {
+    ...mapState(["sportService"])
+  }
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,33 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    sportService: []
+  },
+  mutations: {
+    SET_SPORTSERVICE(state, sportService) {
+      state.sportService = sportService;
+    }
+  },
+  actions: {
+    getSportService({ commit }) {
+      axios
+        .get("https://sportservice.inplaynet.tech/api/sport/getheader/en")
+        .then(res => {
+          // eslint-disable-next-line no-console
+          console.log(res.data);
+          const parsedData = JSON.parse(res.data);
+          commit("SET_SPORTSERVICE", parsedData);
+        })
+        .catch(err => {
+          // eslint-disable-next-line no-console
+          console.log(err);
+        });
+    }
+  },
   modules: {}
 });
